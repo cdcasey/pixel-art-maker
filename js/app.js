a8486ec7397c2fd83d83a01e21b97ed91e6df620
Chris Casey <cdcasey@gmail.com>
2018-02-22 21:06:59 -0600

    Basic paint functionality working.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,34 @@
 let board = document.querySelector('#board');
 let squares = document.querySelectorAll('.inner');
-let paletteContainer = document.querySelector('#palette');
-let currentColor = document.querySelector('#current');
+let palette = document.querySelector('#palette');
+let currentIndicatedColor = document.querySelector('#current');
+let currentColor = null;
+
+const colors = ["AliceBlue",
+"AntiqueWhite",
+"Aqua",
+"Aquamarine",
+"Azure",
+"Beige",
+"Bisque",
+"Black",
+"BlanchedAlmond",
+"Blue",
+"BlueViolet",
+"Brown",
+"BurlyWood",
+"CadetBlue",
+"Chartreuse",
+"Chocolate",
+"Coral",
+"CornflowerBlue",
+"Cornsilk",
+"Crimson",
+"Cyan",
+]
 
 board.addEventListener('click', changeColor);
-paletteContainer.addEventListener('click',  selectColor);
+palette.addEventListener('click',  selectColor);
 
 for (let i = 0; i < 42; i++) {
     let rowDiv = document.createElement('div');
@@ -16,12 +40,20 @@ for (let i = 0; i < 42; i++) {
     }
 }
 
+colors.forEach(color => {
+    let colorNode = document.createElement('div', {id : color, style: "background-color: " + color});
+    colorNode.setAttribute('id', color);
+    colorNode.setAttribute('style', "background-color: " + color);
+    palette.appendChild(colorNode);
+})
+
 function selectColor(e) {
     let target = e.target || e.srcElement;
     console.log("clicked");
 
     console.log(target.id);
-    currentColor.setAttribute('style', 'background-color: ' + target.id);
+    currentIndicatedColor.setAttribute('style', 'background-color: ' + target.id);
+    currentColor = target.id;
 }
 
 function changeColor(e) {
@@ -33,7 +65,8 @@ function changeColor(e) {
 
     // target.style.backgroundColor = colors[target.id];
     // target.style.backgroundColor = "red";
-    target.classList.toggle("red");
+    // target.classList.toggle("red");
+    target.setAttribute('style', 'background-color: ' + currentColor);
     return false;
 }
 
